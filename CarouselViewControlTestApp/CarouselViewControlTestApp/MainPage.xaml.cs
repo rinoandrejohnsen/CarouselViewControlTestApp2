5fc2f4d4f80ac12ccc02289c98feb6571c4394e4
Rino Andre Johnsen <rinoandrejohnsen@gmail.com>
2017-03-22 20:08:17 +0100

    init

---
+++ CarouselViewControlTestApp/CarouselViewControlTestApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CarouselViewControlTestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.BindingContext = this;
+
+            this.NextPageCommand = new Command(async () => await this.NextPage());
+
+            InitializeComponent();
+
+            this.carouselViewControl.ItemTemplate = new PageTemplateSelector();
+
+            this.carouselViewControl.ItemsSource = new List<int>() { 0 }; 
+        }
+
+        public ICommand NextPageCommand { get; set; }
+
+        private async Task NextPage()
+        {
+            var pageIndex = Convert.ToInt32(this.carouselViewControl.ItemsSource[0]);
+
+            await this.carouselViewControl.InsertPage(pageIndex + 1);
+
+            this.carouselViewControl.Position++;
+
+            await Task.Delay(1000);
+
+            await this.carouselViewControl.RemovePage(0);
+        }
+    }
+}
---
+++ CarouselViewControlTestApp/CarouselViewControlTestApp/PageTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CarouselViewControlTestApp
+{
+    public class PageTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var dataTemplate = new DataTemplate(() =>
+            {
+                return new ContentView()
+                {
+                    BackgroundColor = Color.Green,
+                    HeightRequest = 500,
+                    WidthRequest = 400,
+                    VerticalOptions = LayoutOptions.Center,
+                    Content = new Label()
+                    {
+                        Text = Convert.ToString(item),
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        VerticalTextAlignment = TextAlignment.Center,
+                        FontSize = 30
+                    }
+                };
+            });
+
+            return dataTemplate;
+        }
+    }
+}
